f77ced80cb639812323ad5b62fbe8ad175592804
Gurtaj Chhabra <gurtajsingh@live.com>
2020-08-07 11:57:57 -0400

    removed go and added Nodejs

--- src/App.js
+++ src/App.js
@@ -1,55 +1,34 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import NumberFormat from "react-number-format";
 
 function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
+  const [value, setValue] = useState("");
+  const formatPhoneNumber = (numStr) => {
+    const format = "(###) ###-#### ext.####";
+    let hashCount = 0;
+    const formattedNumberAry = format.split("");
+    for (let i = 0; i < format.length; i++) {
+      if (format[i] === "#") {
+        formattedNumberAry[i] = numStr[hashCount];
+        hashCount += 1;
+      }
     }
-    getDate();
-  }, []);
+    const newFormattedNumber = formattedNumberAry.join("").split(" ext.");
+    if (newFormattedNumber[1]) {
+      return formattedNumberAry.join("");
+    }
+    return newFormattedNumber[0];
+  };
   return (
     <main>
-      <h1>Create React App + Go API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/master/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
+      <h1>Create React App + NodeJS API</h1>
+      <NumberFormat
+        format={formatPhoneNumber}
+        type="tel"
+        value={value}
+        autoComplete="no"
+      />
     </main>
   );
 }
